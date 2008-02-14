fb0ac90649dc80ce70d29faa4cab4dac8ddafcef
john <john@e164b5c0-1b64-4604-8c5e-ec2bf4efc65e>
2008-02-14 23:09:00 +0000

    Viewer is now working with labelled surfaces, transparent walls, and axis labels.

--- viewer.cpp
+++ viewer.cpp
@@ -1,7 +1,24 @@
-/**@FILE
+/*	VIEW3D
+	Copyright (C) 2008 John Pye
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
+*//**@FILE
 	Load and parse View3D data file, then render the 3D geometry using OpenGL 
 	in the COIN3D Examiner-Viewer.
-	See http://www.coin3d.org/ as well as http://vSoIndexediew3d.sf.net (this project)
+	See http://www.coin3d.org/ as well as http://view3d.sf.net (this project)
 */
 
 #include <Inventor/Qt/SoQt.h>
@@ -16,6 +33,8 @@
 #include <Inventor/nodes/SoNormal.h>
 #include <Inventor/nodes/SoNormalBinding.h>
 #include <Inventor/nodes/SoIndexedFaceSet.h>
+#include <Inventor/nodes/SoTranslation.h>
+#include <Inventor/nodes/SoText2.h>
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -34,6 +53,8 @@ extern "C"{
 #include "getdat.h"
 #include "polygn.h"
 };
+
+#include "render.h"
 
 #include <sstream>
 #include <stdexcept>
@@ -109,11 +130,8 @@ int main(int argc, char **argv){
 	int nSrf, nSrf0;
 	nSrf = CD.nRadSrf;
 	nSrf0 = CD.nRadSrf;
-	int encl = CD.enclosure;
 
   	if(CD.format == 4)CD.nVertices = 4 * CD.nAllSrf;
-
-	const char *types[]={"rsrf","subs","mask","nuls","obso"};
 
 	/* surface names [1:nSrf][0:NAMELEN] */
 	char **name = (char **)Alc_MC(1, nSrf0, 0, NAMELEN, sizeof(char), __FILE__, __LINE__ );
@@ -146,6 +164,9 @@ int main(int argc, char **argv){
 	for(int n=nSrf; n; n--)area[n] = (float)srf[n].area;
 	NxtClose();
 
+#if 0 
+	const char *types[]={"rsrf","subs","mask","nuls","obso"};
+
     fprintf(stderr, "Surfaces:\n" );
     fprintf( stderr, "   #        name     area   emit  type bsn csn (dir cos) (centroid)\n" );
 	int n;
@@ -169,6 +190,7 @@ int main(int argc, char **argv){
           srf[n].v[j]->x, srf[n].v[j]->y, srf[n].v[j]->z );
       fprintf( stderr, "\n" );
     }
+#endif
 
 #define FAILED_TO_READ_MODEL 0
 	/* FIXME how to determine if there was a problem reading the input file? */
@@ -190,19 +212,21 @@ int main(int argc, char **argv){
 	// FIXME: add materials info
 	SoMaterial *mat = new SoMaterial;
 	mat->diffuseColor.setValue(1,1,0);
+	mat->transparency = 0.5;
 	root->addChild(mat);	
 
 	// vertices 
 	SoCoordinate3 *coo = new SoCoordinate3;
 	SbVec3f *vcoo = new SbVec3f[CD.nVertices];
 	for(int i=0; i<CD.nVertices; ++i){
-		SbVec3f P(xyz[i].x, xyz[i].y, xyz[i].z);
+		SbVec3f P(xyz[i+1].x, xyz[i+1].y, xyz[i+1].z);
 		vcoo[i] = P;
 	}
 	coo->point.setValues(0, CD.nVertices, vcoo);
 	root->addChild(coo);
 
-	fprintf(stderr, "Added %d vertices to face set\n",CD.nVertices);
+	fprintf(stderr, "Created %d vertices.\n",CD.nVertices);
+
 #if 0
 	// surface normals
 	SoNormal *nrm = new SoNormal;
@@ -221,30 +245,64 @@ int main(int argc, char **argv){
 
 	// indices of the vertices for each face
 	vector<int32_t> ids;
+	map<int,SbVec3f> sfclabels;
 	long nvals = 0;
     for(int i=1; i<=CD.nAllSrf; i++){
+		SbVec3f L(0,0,0);
 		for(int j=0; j<srf[i].nv; j++){
 			long id = (long)(srf[i].v[j] - xyz);
-			ids.push_back(id);
-			fprintf(stderr,"srf %d: vert %d = %f, %f, %f\n",i,id,xyz[id].x, xyz[id].y, xyz[id].z);
+			ids.push_back(id-1);
+			//fprintf(stderr,"srf %d: vert %ld = %f, %f, %f (=? %f, %f, %f)\n",i,id,xyz[id].x, xyz[id].y, xyz[id].z, vcoo[id][0], vcoo[id][1], vcoo[id][2]);
+			SbVec3f P(xyz[id].x, xyz[id].y, xyz[id].z);
+			L = L + P;			
 		}
+		L = float(1./srf[i].nv) * L;
+		sfclabels[i] = L;
 		ids.push_back(SO_END_FACE_INDEX);
 		nvals += srf[i].nv + 1;
     }
-	fprintf(stderr,"Added %d faces (nvals = %d) to face set (using %d space)\n",CD.nAllSrf,nvals,ids.size());
+	fprintf(stderr,"Created %d faces.\n",CD.nAllSrf);
 	
 	SoIndexedFaceSet *fset = new SoIndexedFaceSet;
 	fset->coordIndex.setValues(0, nvals, &(ids[0]));
 	root->addChild(fset);	
 
+	// add the surface labels
+
+	SoMaterial *textcolor = new SoMaterial;
+	textcolor->diffuseColor.setValue(1,0,0);
+	root->addChild(textcolor);
+
+#if 1	
+	for(map<int,SbVec3f>::const_iterator mi=sfclabels.begin(); mi!=sfclabels.end(); ++mi){
+		//fprintf(stderr,"Adding label for surface %d = %f, %f, %f\n",mi->first, mi->second[0], mi->second[1], mi->second[2]);
+		SoSeparator *s = new SoSeparator;
+		SoTranslation *tr = new SoTranslation;
+		tr->translation = mi->second;
+		s->addChild(tr);
+		SoText2 *txt = new SoText2;
+		stringstream ss;
+		ss << name[mi->first] << " (e=" << emitt[mi->first] << ")";
+		//fprintf(stderr,"mi->first = %d\n", mi->first);
+		txt->string = SbString(ss.str().c_str());
+		//fprintf(stderr,"Label = %s", ss.str().c_str());
+		s->addChild(txt);
+		root->addChild(s);
+	}		
+#endif
+
+	// add axes
+	root->addChild(axes());
+
 	if(sceneoutfile){
 		// output the scene to a file that can be viewed later
+		fprintf(stderr,"Writing scene to file '%s'...\n",sceneoutfile);
 		SoWriteAction wr;
 		FILE *f;
 		f = fopen(sceneoutfile,"w");
 		if(!f) throw runtime_error("Unable to write to scene output file");
 		wr.getOutput()->setFilePointer(f);
-		fprintf(stderr,"Writing scene file to '%s'...", sceneoutfile);
+		//fprintf(stderr,"Starting write...");
 		wr.apply(root);
 		fprintf(stderr,"done!\n");
 	}else{
