d23be86c0589d39b67cda218fca4a8d9b49cfc27
jdpipe <jdpipe@e164b5c0-1b64-4604-8c5e-ec2bf4efc65e>
2008-02-28 07:09:40 +0000

    Added first cut homepage.

---
+++ htdocs/index.php
@@ -0,0 +1,22 @@
+<html><head><title>View3D: calculation of radiation view factors in 2D and 3D</title></head>
+<body>
+
+<h1>View3D</h1>
+
+<p><b>View3D</b> is a command-line tool for evaluating radiation view factors for scenes with complex 2D and 3D geometry. It uses an adaptive integration method to calculate the view factors between faces where there is partial obstruction from inbetween faces.</p>
+
+<p>View3D was originally developed by George Walton. The project is now maintained by <a href="http://sourceforge.net/users/jdpipe">John Pye</a> and <a href="http://sourceforge.net/users/georgewalton/">George Walton</a>. New cross-platform 2D and 3D viewer code has been developed to replace the original Windows-only code, and a new build script has been added to automate the job of compiling the program on Linux (GCC) and MinGW (GCC).</p>
+
+<ul>
+<li><a href="http://sourceforge.net/projects/view3d">Sourceforge.net Project Page</a>
+<li><a href="http://view3d.svn.sourceforge.net/viewvc/*checkout*/view3d/trunk/README.txt">README.txt</a>
+<li><a href="http://view3d.svn.sourceforge.net/viewvc/*checkout*/view3d/trunk/LICENSE.txt">License</a>
+<li><a href="http://view3d.svn.sourceforge.net/viewvc/*checkout*/view3d/trunk/MANUAL.doc">MANUAL.doc</a> (93k Word document)
+<li><a href="http://view3d.svn.sourceforge.net/viewvc/view3d/trunk/">Browse the code</a>
+</ul>
+
+<a href="http://sourceforge.net/projects/view3d"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=217420&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>
+
+</body>
+</html>
+
