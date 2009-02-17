582c61d1245be1ba88c35c897d9b28040be2e863
jdpipe <jdpipe@e164b5c0-1b64-4604-8c5e-ec2bf4efc65e>
2009-02-17 08:32:45 +0000

    Fixed build on Windows with GTK+ for 2D viewer.cpp Checked OK against the gtk-2.14.7 bundle from gtk.org.

--- scons/soqt.py
+++ scons/soqt.py
@@ -17,6 +17,27 @@ def generate(env):
 			env['SOQT_CPPPATH'] = [INCLUDE]
 			env['SOQT_LIBPATH'] = [LIB]
 			env['SOQT_LIBS'] = ['SoQt']
+			env['SOQT_CPPDEFINES'] = ['SOQT_DLL']
+			
+			x=_winreg.ConnectRegistry(None,_winreg.HKEY_LOCAL_MACHINE)
+			y= _winreg.OpenKey(x,r"SOFTWARE\coin3d")
+			LIB,t = _winreg.QueryValueEx(y,"INSTALL_LIB")
+			BIN,t = _winreg.QueryValueEx(y,"INSTALL_BIN")
+			INCLUDE,t = _winreg.QueryValueEx(y,"INSTALL_INCLUDE")
+			
+			env.AppendUnique(
+				SOQT_CPPPATH = [INCLUDE]
+				,SOQT_LIBPATH = [LIB]
+				,SOQT_LIBS = ['Coin']
+				,SOQT_CPPDEFINES = ['COIN_DLL']
+			)
+			
+			env.AppendUnique(
+				QT_PREFIX = r'c:/Qt/4.3.3'
+				,SOQT_CPPPATH = ["$QT_PREFIX/include","$QT_PREFIX/include/Qt"]
+				,SOQT_LIBPATH = ["$QT_PREFIX/lib"]
+			)
+			
 		else:
 			cmd = ['soqt-config','--cppflags','--ldflags','--libs']
 			env1 = env.Clone()
@@ -24,10 +45,12 @@ def generate(env):
 			env['SOQT_CPPPATH'] = env1.get('CPPPATH')
 			env['SOQT_LIBPATH'] = env1.get('LIBPATH')
 			env['SOQT_LIBS'] = env1.get('LIBS')
+			env['SOQT_CPPDEFINES'] = env1.get('CPPDEFINES')
 
-		#print "SOQT_LIBS =",env.get('SOQT_LIBS')
-		#print "SOQT_LIBPATH =",env.get('SOQT_LIBPATH')
-		#print "SOQT_CPPPATH =",env.get('SOQT_CPPPATH')
+		print "SOQT_LIBS =",env.get('SOQT_LIBS')
+		print "SOQT_LIBPATH =",env.get('SOQT_LIBPATH')
+		print "SOQT_CPPPATH =",env.get('SOQT_CPPPATH')
+		print "SOQT_CPPDEFINES =",env.get('SOQT_CPPDEFINES')
 
 	except:
 		print "FAILED TO SET UP SOQT"
