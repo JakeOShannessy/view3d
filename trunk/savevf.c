22cebc52930f1812c25ae19fc819cfe2ea8d2940
jdpipe <jdpipe@e164b5c0-1b64-4604-8c5e-ec2bf4efc65e>
2009-05-15 04:30:41 +0000

    Added some debug output in savevf.c Updated documentation to clarify current output file format.

--- trunk/savevf.c
+++ trunk/savevf.c
@@ -16,42 +16,48 @@
 /*  Save view factors as square array; + area + emit; text format.  */
 
 static void SaveF0( char *fileName, char *header, int nSrf,
-             float *area, float *emit, double **AF, float *F 
+		float *area, float *emit, double **AF, float *F 
 ){
   FILE *vfout;
-  int n;    /* row */
-  int m;    /* column */
+  int n; /* row */
+  int m; /* column */
 
-  vfout = fopen( fileName, "w" );
-  fprintf( vfout, "%s", header );
-  fprintf( vfout, "%g", area[1] );
-  for( n=2; n<=nSrf; n++ )
-    fprintf( vfout, " %g", area[n] );
-  fprintf( vfout, "\n" );
+  fprintf(stderr,"Saving view factors to file '%s' in format '0' (square array, area, emit)...\n",fileName);
 
-  for( n=1; n<=nSrf; n++ )      /* process AF values for row n */
-    {
+  vfout = fopen(fileName, "w");
+  fprintf(vfout, "%s", header);
+  fprintf(vfout, "%g", area[1]);
+  for( n=2; n<=nSrf; n++ ){
+    fprintf(vfout, " %g", area[n]);
+  }
+  fprintf(vfout, "\n");
+
+  for(n=1; n<=nSrf; n++){
+    /* process AF values for row n */  
     double Ainv = 1.0 / area[n];
-    for( m=1; m<=nSrf; m++ )      /* process column values */
-      {
-      if( m < n )
+    for(m=1; m<=nSrf; m++){
+      /* process column values */
+      if(m < n){
         F[m] = (float)(AF[n][m] * Ainv);
-      else
+      }else{
         F[m] = (float)(AF[m][n] * Ainv);
       }
-    fprintf( vfout, "%.6f", F[1] );   /* write row */
-    for( m=2; m<=nSrf; m++ )
-      fprintf( vfout, " %.6f", F[m] );
-    fprintf( vfout, "\n" );
     }
+    /* write row */
+    fprintf(vfout, "%.6f", F[1]);   
+    for(m=2; m<=nSrf; m++){
+      fprintf( vfout, " %.6f", F[m]);
+    }
+    fprintf(vfout, "\n");
+  }
 
   fprintf( vfout, "%.3f", emit[1] );
-  for( n=2; n<=nSrf; n++ )
+  for( n=2; n<=nSrf; n++ ){
     fprintf( vfout, " %.3f", emit[n] );
+  }
   fprintf( vfout, "\n" );
   fclose( vfout );
-
-}  /* end of SaveF0 */
+} /* end of SaveF0 */
 
 /***  SaveF1.c  **************************************************************/
 
@@ -62,6 +68,8 @@ static void SaveF1( char *fileName, char *header, int nSrf,
 ){
   FILE *vfout;
   int n;    /* row */
+
+  fprintf(stderr,"Saving view factors to file '%s' in format '1' (square array, binary format)...\n",fileName);
 
   vfout = fopen( fileName, "wb" );
   fwrite( header, sizeof(char), 32, vfout );
@@ -128,34 +136,30 @@ void SaveAF( char *fileName, char *header, int nSrf, char *title, char **name,
 
 void SaveVF( char *fileName, char *program, char *version,
              int format, int encl, int didemit, int nSrf,
-             float *area, float *emit, double **AF, float *vtmp )
-  {
+             float *area, float *emit, double **AF, float *vtmp 
+){
   char header[32];
   int j;
 
   /* fill output file header line */
-  sprintf( header, "%s %s %d %d %d %d",
-    program, version, format, encl, didemit, nSrf );
-  for( j=strlen(header); j<30; j++ )
+  sprintf( header, "%s %s %d %d %d %d", program, version, format, encl, didemit, nSrf );
+  for( j=strlen(header); j<30; j++ ){
     header[j] = ' ';
+  }
   header[30] = '\n';
   header[31] = '\0';
 
-  if( format == 0 )  /* simple text file */
-    {
+  if( format == 0 ){  /* simple text file */
     SaveF0( fileName, header, nSrf, area, emit, AF, vtmp );
-    }
-  else if( format == 1 )  /* simple binary file */
-    {
+    
+  }else if( format == 1 ){  /* simple binary file */
     header[30] = '\r';
     header[31] = '\n';
     SaveF1( fileName, header, nSrf, area, emit, AF, vtmp );
-    }
-  else
-    {
+    
+  }else{
     error( 3, __FILE__, __LINE__, "Undefined format: ", IntStr(format), "" );
-//    SaveAF( fileName, header, nSrf, title, name, area, emit, AF );
-    }
-
-  }  /* end SaveVF */
+    // SaveAF( fileName, header, nSrf, title, name, area, emit, AF );
+  }
+} /* end SaveVF */
 
