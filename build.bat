983b6e7f7f8e866a406075289080be8a5fb0fa5a
Jake O'Shannessy <joshannessy@gmail.com>
2018-11-11 00:27:26 +0100

    Hard-code 64 bits

--- build.bat
+++ build.bat
@@ -7,11 +7,11 @@ cd ..
 goto :EOF
 
 :: Run the "vsvarsall.bat" script provided by Visual Studio. This will place
-:: cl.exe, link.exe, into the path.
+:: cl.exe, link.exe, into the path. Currently this is hardcoded to use 64-bit.
 :LoadVCVars
-call "%VS150COMNTOOLS%\..\..\VC\vcvarsall.bat"
+call "%VS150COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
 if not ERRORLEVEL 1 goto DoneLoadVCVars
-call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"
+call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
 if not ERRORLEVEL 1 goto DoneLoadVCVars
 echo "Visual Studio not found, build will likely fail."
 :DoneLoadVCVars
