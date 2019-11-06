eebbf58e9eea0da9d5472fd4e3326d92a2fdaaf9
Jake O`Shannessy <joshannessy@affinity-eng.com>
2019-11-06 18:27:52 +0000

    Update argument names (2d).

--- src/bin/view2d.rs
+++ src/bin/view2d.rs
@@ -9,21 +9,22 @@ fn main() {
         .author("Jake O'Shannessy <joshannessy@gmail.com>")
         .about("Compute view factors for a 2D geometry.")
         .arg(
-            Arg::with_name("INFILE")
+            Arg::with_name("INPUT-FILE")
                 .help("The input file")
-                .value_name("INFILE"),
+                .value_name("INPUT-FILE")
+                .required(true),
         )
         .arg(
-            Arg::with_name("OUTFILE")
+            Arg::with_name("OUTPUT-FILE")
                 .help("The output file")
-                .value_name("OUTFILE"),
+                .value_name("OUTPUT-FILE"),
         )
         .get_matches();
 
-    let infile = matches.value_of("INFILE").expect("Input file not provided");
+    let infile = matches.value_of("INPUT-FILE").expect("Input file not provided");
     // If no outfile is specified, pass an empty string and the library will
     // understand that it means direct to stdout
-    let outfile = matches.value_of("OUTFILE").unwrap_or("");
+    let outfile = matches.value_of("OUTPUT-FILE").unwrap_or("");
     // Convert these arguments to C strings to use in FFI
     let infile_c = CString::new(infile).expect("CString::new failed");
     let outfile_c = CString::new(outfile).expect("CString::new failed");
